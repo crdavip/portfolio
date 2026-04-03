6ed6e59332eaa2f2c231bc920f0925c37a19ebe0
crdavip <crdavip@gmail.com>
2026-04-03 12:39:14 -0500

    Update critical project packages

--- src/components/ui/portfolio/Portfolio.tsx
+++ src/components/ui/portfolio/Portfolio.tsx
@@ -2,13 +2,22 @@ import { useTranslations } from "next-intl";
 import { PortfolioCard } from "./PortfolioCard";
 import { portfolio } from "@/data";
 import { PorfolioItem } from "@/interfaces";
+import { Brush, CodeXml } from "lucide-react";
 
 export const Portfolio = () => {
   const portfolioItems: PorfolioItem[] = portfolio;
   const t = useTranslations("Portfolio");
 
+  const categoryIcon = {
+  development: <CodeXml size={18} />,
+  design: <Brush size={18} />,
+};
+
   return (
-    <section className="max-w-6xl mx-auto h-fit flex flex-col justify-center items-center mt-25 sm:mt-50 px-4 sm:px-8 xl:px-0" id="portfolio-section">
+    <section
+      className="max-w-6xl mx-auto h-fit flex flex-col justify-center items-center mt-25 sm:mt-50 px-4 sm:px-8 xl:px-0"
+      id="portfolio-section"
+    >
       <h3 className="text-center">{t("h3")}</h3>
       <div className="mt-15 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {portfolioItems.map((portfolio) => (
